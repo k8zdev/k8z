3260e94470de1da74ae5f0a1137d961e324e416d
疯魔慕薇 <kfanjian@gmail.com>
2024-02-08 21:16:36 +0800

    add methods of K8zService
    Signed-off-by: 疯魔慕薇 <kfanjian@gmail.com>

---
+++ lib/services/k8z_service.dart
@@ -0,0 +1,52 @@
+import 'package:k8sapp/dao/kube.dart';
+import 'package:k8sapp/services/k8z_native.dart';
+
+/// [K8zService] implements a service to interactiv with Kubernetes cluster.
+///
+/// To use [K8zService] must provide cluster info when init this service.
+class K8zService {
+  K8zCluster cluster;
+  String proxy;
+  int timeout;
+
+  K8zService({
+    required this.cluster,
+    this.proxy = "",
+    this.timeout = 60,
+  });
+
+  /// [checkHealth] used to check the Kubernetes cluster is health.
+  ///
+  Future<bool> checkHealth() async {
+    var resp =
+        K8zNative().k8zRequest(cluster, proxy, timeout, "GET", "/readyz", "");
+    if (resp.body == "ok") {
+      return true;
+    }
+    return false;
+  }
+
+  /// [get] it is used to run the GET request on the Kubernetes cluster.
+  ///
+  Future<BodyReturn> get(String api) async {
+    return K8zNative().k8zRequest(cluster, proxy, timeout, "GET", api, "");
+  }
+
+  /// [delete] it is used to run the DELETE request on the Kubernetes cluster.
+  ///
+  Future<BodyReturn> delete(String api, String body) async {
+    return K8zNative().k8zRequest(cluster, proxy, timeout, "DELETE", api, body);
+  }
+
+  /// [patch] it is used to run the PATCH request on the Kubernetes cluster.
+  /// The [body] must be a valid json patch.
+  Future<BodyReturn> patch(String api, String body) async {
+    return K8zNative().k8zRequest(cluster, proxy, timeout, "PATCH", api, body);
+  }
+
+  /// [post] it is used to run the POST request on the Kubernetes cluster.
+  ///
+  Future<BodyReturn> post(String api, String body) async {
+    return K8zNative().k8zRequest(cluster, proxy, timeout, "POST", api, body);
+  }
+}
