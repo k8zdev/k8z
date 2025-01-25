f36f242515598b42346e7bc7f955181a24f1d2d1
疯魔慕薇 <kfanjian@gmail.com>
2025-01-25 23:15:00 +0800

    update and fix 1. update ios and macos dependency. 2. fix macos Command PhaseScriptExecution failed with a nonzero exit code.
    Signed-off-by: 疯魔慕薇 <kfanjian@gmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,9 +14,9 @@ import in_app_review
 import package_info_plus
 import path_provider_foundation
 import purchases_flutter
-import screen_retriever
+import screen_retriever_macos
 import share_plus
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import window_manager
 
@@ -30,7 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
